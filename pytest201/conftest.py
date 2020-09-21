d100f5aeb3e486ae4cb29aac2840cda78ad5836b
Bin Chen <binchen.devops@gmail.com>
2020-09-20 21:24:47 -0400

    added pytest301 demo

--- pytest201/conftest.py
+++ pytest201/conftest.py
@@ -2,10 +2,12 @@ import pytest
 
 @pytest.fixture
 def my_fixture():
+    """give constant 42"""
     return 42
 
 
 @pytest.fixture
 def captured_print(capsys):
+    """fixture using inbuilt fixture "capsys". """
     print("hello")
 
